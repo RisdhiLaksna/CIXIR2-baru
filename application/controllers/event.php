8a04dced442ae9bf31ac4ee359e7b362664f4f30
RisdhiLaksna <risdhi_laksna_26rpl@student.smktelkom-mlg.sch.id>
2019-01-29 14:38:28 +0700

    Minus Logout

---
+++ application/controllers/event.php
@@ -0,0 +1,10 @@
+<?php  
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Event extends CI_Controller {
+	public function index(){
+		$data['konten']="v_event";
+		$this->load->view('template', $data);
+	}
+}
+?>
---
+++ application/views/v_event.php
@@ -0,0 +1,57 @@
+<section class="wrapper">
+          	<h3 style="color: #000;"><i class="fa fa-angle-right" style="color: #000;"></i> My Event</h3>
+              <!-- page start-->
+              <div class="row mt">
+                  <aside class="col-lg-3 mt">
+                      <h4 style="color: #000;"><i class="fa fa-angle-right"></i> Draggable Events</h4>
+                      <div id="external-events">
+                          <div class="external-event label label-theme">My Event 1</div>
+                          <div class="external-event label label-success">My Event 2</div>
+                          <div class="external-event label label-info">My Event 3</div>
+                          <div class="external-event label label-warning">My Event 4</div>
+                          <div class="external-event label label-danger">My Event 5</div>
+                          <div class="external-event label label-default">My Event 6</div>
+                          <div class="external-event label label-theme">My Event 7</div>
+                          <div class="external-event label label-info">My Event 8</div>
+                          <div class="external-event label label-success">My Event 9</div>
+                          <p class="drop-after" style="color: #000;">
+                              <input type="checkbox" id="drop-remove">
+                              Remove After Drop
+                          </p>
+                      </div>
+                  </aside>
+                  <aside class="col-lg-3 mt">
+                      <section class="panel">
+                          <div class="panel-body">
+                             <p>My Journal Event 1</p>
+                             <div class="photo">
+                                <a class="fancybox" href="<?= base_url()?>assets/aa.jpg"><img class="img-responsive" style="width: 250px;" src="<?= base_url()?>assets/aa.jpg" alt=""></a>
+                            </div>
+                          </div>
+                      </section>
+                  </aside>
+
+                  <aside class="col-lg-3 mt">
+                      <section class="panel">
+                          <div class="panel-body">
+                             <p>My Journal Event 2</p>
+                             <div class="photo">
+                                <a class="fancybox" href="<?= base_url()?>assets/bb.jpg"><img class="img-responsive" style="width: 250px;" src="<?= base_url()?>assets/bb.jpg" alt=""></a>
+                            </div>
+                          </div>
+                      </section>
+                  </aside>
+
+                  <aside class="col-lg-3 mt">
+                      <section class="panel">
+                          <div class="panel-body">
+                             <p>My Journal Event 3</p>
+                             <div class="photo">
+                                <a class="fancybox" href="<?= base_url()?>assets/cc.jpg"><img class="img-responsive" style="width: 250px;" src="<?= base_url()?>assets/cc.jpg" alt=""></a>
+                            </div>
+                          </div>
+                      </section>
+                  </aside>
+              </div>
+              <!-- page end-->
+		</section><! --/wrapper -->
